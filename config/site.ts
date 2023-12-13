d869f686d4b06d5acd7a7ed940567e90ec151d79
Mattbln <matthieu.belin@epitech.digital>
2023-12-13 15:00:25 +0100

    commit PWA

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "DYCALL",
+  description:
+    "DYCALL permet aux entreprises de rendre le cold calling (prospection téléphonique) ludique et mesurable avec des jeux et des statistiques.",
+  url: "https://prepare-mattbln.vercel.app/",
+  ogImage: "https://dycall.com/og.jpg",
+  links: {
+    twitter: "https://twitter.com/joris_delorme_",
+    github: "https://github.com/joris-delorme/DYCALL",
+  },
+};
